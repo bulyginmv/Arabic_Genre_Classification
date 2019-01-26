0e48a25293e439255579444eea6cee39349e8533
bulyginmv <45527757+bulyginmv@users.noreply.github.com>
2019-01-25 19:43:49 -0500

    Update SVM_3gram_Classification.py

--- SVM_3gram_Classification.py
+++ SVM_3gram_Classification.py
@@ -10,17 +10,19 @@ import re
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn import svm
+
+#opening and spliting training corpus
 arabic_corpus = open('ARcorpusALL.txt', encoding='utf-8-sig')
-test_corpus=open('test_X.txt', encoding='utf-8-sig')
-test_corpus=test_corpus.read()
-test_corpus=test_corpus.split('text*')
 arabic_corpus=arabic_corpus.read()
 arabic_corpus=re.split(r'تكست إد',arabic_corpus)
 arabic_corpus.pop(0)
-count_vect = CountVectorizer(analyzer='char_wb', ngram_range=(3, 3))
-X_train_counts = count_vect.fit_transform(arabic_corpus)
-tf_transformer = TfidfTransformer(use_idf=False).fit(X_train_counts)
-X_train_tf = tf_transformer.transform(X_train_counts)
+
+#opening and spliting test corpus
+test_corpus=open('test_X.txt', encoding='utf-8-sig')
+test_corpus=test_corpus.read()
+test_corpus=test_corpus.split('text*')
+
+#opening and spliting target file
 genre_scores = open('annotations.txt','r',encoding='utf-8')
 genre_scores = genre_scores.read()
 genre_scores = genre_scores.split('\n')
@@ -28,16 +30,25 @@ y_train=[]
 for score in genre_scores:
     y_train.append(score.split('\t'))
 y_train=np.array(y_train)
+
+#preprocessing of the training and test corpora
+count_vect = CountVectorizer(analyzer='char_wb', ngram_range=(3, 3))
+X_train_counts = count_vect.fit_transform(arabic_corpus)
+tf_transformer = TfidfTransformer(use_idf=False).fit(X_train_counts)
+X_train_tf = tf_transformer.transform(X_train_counts)
 X_new_counts = count_vect.transform(test_corpus)
 X_new_tfidf = tf_transformer.transform(X_new_counts)
 results=[]
 clf = svm.LinearSVC()
 
+#the actual training and predicting
 for i in range (18):
     clf.fit(X_train_tf, y_train[:,i])
     results.append(clf.predict(X_new_tfidf))
 results_final=np.array(results)
 results_final=results_final.transpose()
+
+#Saving predictions in the txt file
 k = open('resultsvmtr.txt','tw', encoding='utf-8')
 for x in results_final:
     k.write('\t'.join([str(y) for y in x]))
